0fe3bdf51c375dd91ae3549950a6cc763c7e719c
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-01-03 23:33:14 +0500

    Determining American to British elevator System

---
+++ Elevator.py
@@ -0,0 +1,5 @@
+# Convert elevator floors
+inp = input('What is your Floor = ')#input prints out the prompt and pauses and wait
+#input string ki form ma ata ha and 'inp varibale ma store hojata ha'
+usf = int(inp) + 1 #converting string to integer
+print('USA Floor', usf)
