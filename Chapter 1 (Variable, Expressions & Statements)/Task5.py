c5af72be16fe6f620e0886f18be7ff82808a0e3f
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-05-02 15:49:56 +0500

    Finding the version of python
    Write a Python program to find out what version of Python you are using.

---
+++ Chapter 1 (Variable, Expressions & Statements)/Task5.py
@@ -0,0 +1,11 @@
+#Question:
+
+"""Write a Python program to find out what version of Python you are using."""
+
+#Code:
+
+import sys
+print("Python Version")
+print(sys.version)
+print("Version Info")
+print(sys.version_info)
