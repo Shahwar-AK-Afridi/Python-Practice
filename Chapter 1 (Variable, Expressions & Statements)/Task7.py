19802cd2566ba1c0919f129eee6ceea6130f1eb6
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-05-02 15:59:06 +0500

    Print Name in Reverse Order
    Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.

---
+++ Chapter 1 (Variable, Expressions & Statements)/Task7.py
@@ -0,0 +1,11 @@
+#Question
+
+"""
+Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+"""
+
+#Code
+fname = input("Enter Your First Name: ")
+lname = input("Enter Your Last Name: ")
+print("Name in Normal Order: " + fname +" "+ lname)
+print("Name in reverse Order: " +lname + " " + fname)
