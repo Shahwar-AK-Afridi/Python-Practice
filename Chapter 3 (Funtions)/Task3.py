0a15ffcf23c1657473cc05faa04743cb36b6ef5e
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-05-04 14:28:01 +0500

    Using built-in functions
    Using built-in functions max(), min(), int() ,float()

---
+++ Chapter 3 (Funtions)/Task3.py
@@ -0,0 +1,28 @@
+#Using built-in functions
+
+sub1 = "Hello_world"
+maximum = max(sub1)
+print(maximum)
+
+minimum = min(sub1)
+print(minimum)
+
+#Calculating length of string
+length = len(sub1)
+print(length)
+
+#Float to Integer
+num1 = -2.56
+integer = int(num1)
+print(integer)
+
+#Integer to Float
+num2 = 94
+flot = float(num2)
+print(flot)
+
+#String to Float
+sub2 = "345"
+flot = float(sub2)
+add = flot + 1
+print(add)
