4d59a73f372fce5658e077bc527be8c6620e82fd
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-05-04 16:10:38 +0500

    Revoking Function and Passing Residual Value using return statement
    Using return statement to pass residual value

---
+++ Chapter 3 (Funtions)/Task4.py
@@ -0,0 +1,18 @@
+#First: Just Revoking or calling a function
+
+def print_lyrics():          #Header
+    print("Hello World")
+    print("Hello World")
+    return"hello"
+
+# a will save the residual value and then prints it however print() function doesnot need to be returned
+a = print_lyrics() #hello will be assigned to a 
+print(a)
+
+#Second: Using function within function
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+    return
+
+repeat_lyrics()
