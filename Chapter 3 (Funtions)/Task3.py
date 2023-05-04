4723b01688334095d80fe84a6173ae4e528794b0
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-05-04 15:40:53 +0500

    Using built-in functions
    Using type(), max(), min(), str(), int(), float()

--- Chapter 3 (Funtions)/Task3.py
+++ Chapter 3 (Funtions)/Task3.py
@@ -2,27 +2,40 @@
 
 sub1 = "Hello_world"
 maximum = max(sub1)
-print(maximum)
+print("Biggest:",maximum)
 
 minimum = min(sub1)
-print(minimum)
+print("Smallest:",minimum)
 
 #Calculating length of string
 length = len(sub1)
-print(length)
+print("Length:",length)
 
-#Float to Integer
+#float to Integer
 num1 = -2.56
 integer = int(num1)
-print(integer)
+print("Integer:",integer,"is",type(integer))
 
-#Integer to Float
+
+#Integer to float
 num2 = 94
-flot = float(num2)
-print(flot)
+flot1 = float(num2)
+print("Float:",flot1 ,"is",type(flot1))
 
-#String to Float
+
+#String to float
 sub2 = "345"
-flot = float(sub2)
-add = flot + 1
-print(add)
+flot2 = float(sub2)
+print("Float:",flot2,"is",type(flot2))
+
+
+#Integer to String
+num3 = 99
+string1 = str(num3)
+print("String:",string1,"is",type(string1))
+
+
+#Float to String
+num4 = 102.45633
+string2 = str(num4)
+print("String:",string2,"is",type(string2))
