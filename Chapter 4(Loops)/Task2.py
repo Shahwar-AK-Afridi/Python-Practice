4d0e97a2cb7c89335fe399d21e388ae465115bc2
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-05-16 21:24:03 +0500

    Print name using input() and while loop
    Print the name of user according to user input

---
+++ Chapter 4(Loops)/Task2.py
@@ -0,0 +1,10 @@
+name = input("Enter Your Name:")
+times = input("Printing Number:")
+num = int(times)
+count = 0
+while num > 0:
+    print("Hello, %s" %name)
+    num = num - 1
+    count = count + 1
+print("Thank You")
+print("We printed Your name %d times" %count)
