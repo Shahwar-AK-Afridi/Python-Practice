e4c40b341390bb7f505df87f39c3ffee169ae963
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-05-16 23:11:33 +0500

    Practicing For Loop
    For Loop with
    1) Integers
    2) String

---
+++ Chapter 4(Loops)/Task7.py
@@ -0,0 +1,12 @@
+"""Practising For Loop"""
+
+#First
+for i in [5,4,3,2,1]: #No Logical Condition to STOP
+    print("i =",i)    #No of iteration = No of items in the list
+print("Done")
+
+#Second
+for i in ["Apple", "Banana", "Mango"]:
+    print("i =",i)  #Execute Only three times
+    continue
+print("Done")
