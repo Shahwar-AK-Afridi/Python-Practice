d2a4f72c77480fae602e791f0dba169aa9b74d47
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-05-16 21:10:13 +0500

    Using find() method and slicing
    Write code using find() and string slicing to extract the number at the end of the line below.
    Convert the extracted value to a floating point number and print it out.

--- Chapter 5(Strings)/Task1.py
+++ Chapter 5(Strings)/Task1.py
@@ -4,7 +4,7 @@ Convert the extracted value to a floating point number and print it out."""
 text = "X-DSPAM-Confidence:0.8475"
 start = text.find(":")
 print(start)
-extract = text[start+1:]
+extract = text[start+1:] #Starting from index of colon to the end of the string
 print("The string is %s" %extract)
 print("Converting string to float by adding 42.0")
 value = float(extract) + 42.0
