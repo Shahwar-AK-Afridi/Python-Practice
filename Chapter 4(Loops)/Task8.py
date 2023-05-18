869e00f49e59d609837789827c17b34625875986
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-05-18 22:04:05 +0500

    Sum of all numbers
    1)Calculating Sum through For-loop
    2)Calculating Sum of numbers through self-developed function
    3)Calculating Sum through built-in function Sum()

--- Chapter 4(Loops)/Task8.py
+++ Chapter 4(Loops)/Task8.py
@@ -1,9 +1,32 @@
-""""Summing in Loop"""
+"""Calculating Sum through For-loop"""
+
+print("Calculating Sum through For-loop")
 total = 0                           #Iteration variable = "Thing"
 print("Sum Before:%d" %total)
-for sum in [9,41,12,3,74,15]:
+for add in [9,41,12,3,74,15]:
     #Running Total = "total"
-    total = total + sum        #Inside Loop = we add actual number to the running total during each Iteration
+    total = total + add             #Inside Loop = we add actual number to the running total during each Iteration
     # total aka accumulator
-    print(total)
-print("Final Total :%d" %total)   #This total is called as "Overall total of the values"
+print("Final Total :%f" %total)   #This total is called as "Overall total of the values"
+print("-----------------------------------------------")
+
+"""Calculating Sum of numbers through self-developed function"""
+
+print("Calculating Sum of numbers through self-developed function")
+def Summing(numbers):
+    total = 0
+    for add in numbers:
+        total = total + add
+    print("Total Sum: %f" %total)
+    return
+
+numbers = [9,41,12,3,74,15]
+Summing(numbers)
+print("-----------------------------------------------")
+
+"""Calculating Sum through built-in function Sum()"""
+
+print("Calculating Sum through built-in function Sum()")
+num = [9,41,12,3,74,15]
+total = sum(num)
+print("Sum: %f" %total)
