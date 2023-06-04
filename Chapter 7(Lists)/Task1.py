8371af0f9f86aa5f6e0e7c934eba122b00312671
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-06-04 23:56:53 +0500

    lists

---
+++ Chapter 7(Lists)/Task1.py
@@ -0,0 +1,24 @@
+"""Practicing Lists"""
+"""1)Creating lists"""
+
+#First
+print("Creating Lists")
+thislist = ["apple", "banana", "mango"]                          #Three different strings assigned to single variable
+print("First:", thislist)
+
+numbers = [1,10,100]
+print("Second:", numbers)
+
+empty = []
+print("Third:", empty)                                          #Empty List
+
+nested = ["apple", "mango", [1,4,100],"pinapple"]               #Nested List
+print("Fourth:", nested)
+
+fruit =["apple", "mango","banana"]
+vegetable = ["potato", "onion", "green onion"]
+print("Fifth:", fruit, vegetable)
+
+
+
+print("----------------------------------------------------------")
