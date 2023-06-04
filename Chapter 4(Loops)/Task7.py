ccf3ecb65900cb2913be42b94f5cc06ef6398ebd
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-06-04 23:33:35 +0500

    Practicing Loop

--- Chapter 4(Loops)/Task7.py
+++ Chapter 4(Loops)/Task7.py
@@ -1,12 +1,25 @@
-"""Practising For Loop"""
+"""Practicing For Loop"""
 
 #First
 for i in [5,4,3,2,1]: #No Logical Condition to STOP
     print("i =",i)    #No of iteration = No of items in the list
 print("Done")
 
+print("------------------------------------------------")
+
 #Second
 for i in ["Apple", "Banana", "Mango"]:
     print("i =",i)  #Execute Only three times
-    continue
 print("Done")
+
+print("------------------------------------------------")
+
+#Third
+string = "Hello World"
+print("The string is %s" %string)
+count = 0
+for i in string:
+    count = count + 1
+print("The Length of string is %d" %count)
+
+print("------------------------------------------------")
