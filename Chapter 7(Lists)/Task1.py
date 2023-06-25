635a2d533d7ed4582fd85f8eba6281f78912b6e1
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-06-26 04:36:42 +0500

    Practicing Lists
       1)Creating lists
       2)Calculating the Length Of List using => len() & for loop
       3)Type of list using => type()
       4)Creating list using => list() constructor
       5)Combining list using => (+) operator
       6)Repeats a list using => (*) operator
       7)Accessing list elements using => bracket/index operator[](Positive Index Value) & Slice[:] operator
       8)Accessing list elements using => bracket/index operator[](Negative Index Value) & Slice[:] operator
       9)Changing list items using => bracket/index[] operator & slice[:] operator
      10)Traversing a list using => for-loop only to (READ only)
      11)Traversing a list using => for-loop with range() & Len() function to (READ, UPDATE & WRITE)
      12)Searching for elements in a list using => (in & not in) operator

--- Chapter 7(Lists)/Task1.py
+++ Chapter 7(Lists)/Task1.py
@@ -1,16 +1,18 @@
 """Practicing Lists"""
-"""1)Creating lists
-   2)Calculating the Length Of List using (len() & for loop)
-   3)Type of list using (type())
-   4)Creating list using (list() constructor)
-   5)Combining list using (+) operator
-   6)Repeats a list using (*) operator
-   7)Accessing list elements using bracket/index operator[](Positive Index Value) & Slice[:] operator
-   8)Accessing list elements using bracket/index operator[](Negative Index Value) & Slice[:] operator
-   9)Changing list items using bracket/index[] operator & slice[:] operator
-  10)Traversing a list using for-loop only to (READ only)
-  11)Traversing a list using for-loop with range() & Len() function to (READ, UPDATE & WRITE)
-  12)Searching for elements in a list using (in & not in) operator"""
+"""
+   1)Creating lists
+   2)Calculating the Length Of List using => len() & for loop
+   3)Type of list using => type()
+   4)Creating list using => list() constructor
+   5)Combining list using => (+) operator
+   6)Repeats a list using => (*) operator
+   7)Accessing list elements using => bracket/index operator[](Positive Index Value) & Slice[:] operator
+   8)Accessing list elements using => bracket/index operator[](Negative Index Value) & Slice[:] operator
+   9)Changing list items using => bracket/index[] operator & slice[:] operator
+  10)Traversing a list using => for-loop only to (READ only)
+  11)Traversing a list using => for-loop with range() & Len() function to (READ, UPDATE & WRITE)
+  12)Searching for elements in a list using => (in & not in) operator
+"""
 
 #First
 print("=>(1)Creating Lists")
@@ -32,7 +34,7 @@ print("Fifth:", fruit, vegetable)
 print("----------------------------------------------------------")
 
 #Second
-print("=>(2)Calculating the Length Of List using (len() & for loop)")
+print("=>(2)Calculating the Length Of List using => len() & for loop")
 print("-------------Approach 1 using Len() function--------------")
 thislist = ["apple", "mango", "banana", "peach"]
 length = len(thislist)                                                          #Len() function takes list as parameter and returns the number of elements in the list
@@ -46,19 +48,19 @@ print("Length:%d" %count)
 print("----------------------------------------------------------")
 
 #Third
-print("=>(3)Type of list using (type())")
+print("=>(3)Type of list using => type()")
 vegetable = ["potato", "onion", "green onion"]
 print("type:",type(vegetable))                                                  #list is a datatype
 print("----------------------------------------------------------")
 
 #fourth
-print("=>(4)Creating list using (list[] constructor)")
+print("=>(4)Creating list using => list[] constructor")
 hello = list(("Hp", "Lenovo", "Toshiba", "Dell"))
 print(hello)
 print("----------------------------------------------------------")
 
 #Fifth
-print("=>(5)Combining list using (+) operator")
+print("=>(5)Combining list using => (+) operator")
 list1 = ["a","b","c"]
 list2 = [1,2,3]
 list3 = list1 + list2
@@ -66,7 +68,7 @@ print("Concatinated List:", list3)
 print("----------------------------------------------------------")
 
 #Sixth
-print("=>(6)Repeats a list using (*) operator")
+print("=>(6)Repeats a list using => (*) operator")
 list1 = ["mango"] * 5
 print(list1)
 list2 = [1,3,"apple"] * 5
@@ -74,7 +76,7 @@ print(list2)
 print("----------------------------------------------------------")
 
 #Seventh
-print("=>(7)Accessing list elements using bracket/index operator[](Positive Index Value) & Slice[:] operator")
+print("=>(7)Accessing list elements using => bracket/index operator[](Positive Index Value) & Slice[:] operator")
 company = ["Hp", "Lenovo", "Toshiba", "Dell", "ASUS"]
 com1 = company[2]
 print("Company:", com1)
@@ -85,7 +87,7 @@ print("Companies:", com2)
 print("----------------------------------------------------------")
 
 #Eighth
-print("=>(8)Accessing list elements using bracket/index operator[](Negative Index Value) & Slice[:] operator")
+print("=>(8)Accessing list elements using => bracket/index operator[](Negative Index Value) & Slice[:] operator")
 company = ["Hp", "Lenovo", "Toshiba", "Dell", "ASUS"]
 com3 = company[-1]                                                              #-1 refers to the last item
 print("Company:", com3)
@@ -96,7 +98,7 @@ print("Companies:", com4)
 print("----------------------------------------------------------")
 
 #Ninth
-print("=>(9)Changing list items using bracket/index[] operator & slice[:] operator")
+print("=>(9)Changing list items using => bracket/index[] operator & slice[:] operator")
 print("Change item value one by one")
 company = ["Hp", "Lenovo", "Toshiba", "Dell", "ASUS", "Acer", "Samsung"]        #list is mutable
 print("Before:",company)
@@ -106,31 +108,37 @@ company[3] = "peach"
 company[4] = "Dragon Fruit"
 print("After:",company)
 
-print("Change Range of item values using slice[:] operator")
+print("Change Range of item values using => slice[:] operator")
 
 print("----PART A (Equal items inserted and replaced)----")
+
 phone = ["Samsung", "Blackberry", "Apple", "Oppo", "Vivo", "Huawei", "Amazon"]
 print("Before", phone)
+
 #Equal items inserted and replaced
 phone[0:3] = ["Toyota", "Honda", "Suzuki"]
 print("After:", phone)
 
 print("----PART B (More items inserted than replaced)----")
+
 company = ["Hp", "Lenovo", "Toshiba", "Dell", "ASUS", "Acer", "Samsung"]        #list is mutable
 print("Before:", company)
 length_before = len(company)
 print("Length Before:", length_before)
+
 #More items inserted than replaced
 company[0:3] = ["Toyota", "Honda", "Suzuki", "peugeot", "Hyundai"]              #
 print("After:", company)
 length_after = len(company)
 print("Length After:", length_after)
 
-print("----PART C(Less items inserted than replaced)----")
+print("----PART C (Less items inserted than replaced)----")
+
 fruit = ["apple", "mango", "banana", "dragon fruit", "peach" ,"appricot"]
 print("Before:", fruit)
 length_before = len(fruit)
 print("Length Before:", length_before)
+
 #Less items inserted than replaced
 fruit[1:6] = ["Watermelon"]
 print("After:", fruit)
@@ -139,7 +147,7 @@ print("Length After:", length_after)
 print("----------------------------------------------------------")
 
 #Tenth
-print("=>(10)Traversing a list using for-loop only to (READ only)")
+print("=>(10)Traversing a list using => for-loop only to (READ only)")
                                                                                 #Good for "READING" the elements of list ONLY
 for friend in ["joseph", "glenn", "sally"]:                                     #iteration variable "i" go-through the actual elements of the list
     print("Happy New Year:", friend)
@@ -154,14 +162,14 @@ print("Total companies in market: %d" %count)
 print("----------------------------------------------------------")
 
 #Eleventh
-print("=>(11)Traversing a list using for-loop with range() & Len() function to (READ, UPDATE & WRITE)")
+print("=>(11)Traversing a list using => for-loop with range() & Len() function to (READ, UPDATE & WRITE)")
 friends = ["John", "Simon", "Ben"]
 #counted loop or index loop
 for i in range(len(friends)):                                                   #Good for "READING", "WRITING", "UPDATING" the elements of list
     name = friends[i]                                                           #iteration variable "i" go-through the index of the list
     print("Hello:", name)
 
-print("=>Traversing A list using for loop with range() & Len() function")
+print("=>Traversing A list using => for loop with range() & Len() function")
 company = ["Hp", "Lenovo", "Toshiba", "Dell", "ASUS", "Acer", "Samsung"]
 for i in range(len(company)):                                                   #with the help of range() function we can correspond to index position of list.
     print("Company at index (%d): %s" %(i, company[i]))                         #length() returns the number of elements[7]
@@ -169,7 +177,7 @@ for i in range(len(company)):                                                   
 print("----------------------------------------------------------")
 
 #Twelfth
-print("=>(12)Searching for elements in a list using (in & not in) operator")
+print("=>(12)Searching for elements in a list using => (in & not in) operator")
 company = ["Hp", "Lenovo", "Toshiba", "Dell", "ASUS", "Acer", "Samsung"]
 if "Hp" in company:
     print("Yes, Hp is in the list")
