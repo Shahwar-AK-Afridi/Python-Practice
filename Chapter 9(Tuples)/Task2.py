bd37871642d069787249046c437cd36caa822d59
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-06-26 21:55:20 +0500

    Practicing Tuples for adding items & Changing Items
       1)Convert Tuple => To List => append() to add items => Convert Back To Tuple
       2)Add tuple to tuple
       3)Convert Tuple => To List => bracket operator[] to change item => Convert Back To Tuple

--- Chapter 9(Tuples)/Task2.py
+++ Chapter 9(Tuples)/Task2.py
@@ -1,7 +1,8 @@
-"""Practicing Tuples for adding items"""
+"""Practicing Tuples for adding items & Changing Items"""
 """
-   1)Convert Tuple => To List => append() => Convert Back To Tuple
+   1)Convert Tuple => To List => append() to add items => Convert Back To Tuple
    2)Add tuple to tuple
+   3)Convert Tuple => To List => bracket operator[] to change item => Convert Back To Tuple
 """
 
 #First
@@ -31,4 +32,24 @@ add = ("cena",)
 
 name = name + add
 print("Tuple After Adding:", name)
+print("----------------------------------------------------------")
+
+#Third
+print("=>(3)Convert Tuple => To List => bracket operator[] to change item => Convert Back To Tuple")
+
+Vegatable = ("Lemon", "Tomato", "Cucumber", "Onion")
+print("Tuple Before Changing:", fruit)
+
+#Converting "Tuple" to "List"
+lst = list(Vegatable)
+print("Before Changing List:", lst)
+lst[0] = 2
+lst[1] = "Green Onion"
+lst[2] = True
+lst[3] = "Mango"
+print("After Changing List:", lst)
+
+#Converting "List" to "Tuple"
+Vegatable = tuple(lst)
+print("Tuple After Changing:", Vegatable)
 print("----------------------------------------------------------")
