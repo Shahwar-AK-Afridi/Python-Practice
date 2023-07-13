a1bdf05ea4de3c6740d41d012d0675fbe077d751
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-07-13 20:49:51 +0500

    Search an item Using Boolean Variable

--- Chapter 4(Loops)/Task9.py
+++ Chapter 4(Loops)/Task9.py
@@ -1,43 +1,10 @@
-import statistics
+""""Search an item Using Boolean Variable"""
 
-"""Calculating Average through For loop"""
-
-print("Calculating Average through loop")
-total = 0
-denominator = 0
-for num in [9,41,12,3,74,15]:
-    total = total + num
-    denominator = denominator + 1
-print("Total Sum: %d" %total)
-print("Total Numbers: %d" %denominator)
-
-Average = float(total/denominator)
-print("Average: %f" %Average)
-print("-----------------------------------------------")
-
-"""Calculating Average through self-developed function"""
-
-print("Calculating Average through self-developed function")
-def average(number):
-    total = 0
-    denominator = 0
-    for num in number:
-        total = total + num
-        denominator = denominator + 1
-    print("Total Sum: %d" %total)
-    print("Total Numbers: %d" %denominator)
-    Average = float(total/denominator)
-    print("Average: %f" %Average)
-    return
-
-number = [9,41,12,3,74,15]
-average(number)
-print("-----------------------------------------------")
-
-"""Calculating Average through built-in function mean()"""
-
-print("Calculating Average through built-in function mean()")
-number = [9,41,12,3,74,15]
-Average = statistics.mean(number)
-print("Average: %f" %Average)
-print("-----------------------------------------------")
+number = [9,41,12,3,74,15,98,44,57,32,16]
+for obj in number:
+    found = False        #Boolean Variable
+    if obj == 3:
+        found = True
+        print("Number is %d so Found: %s" %(obj, found))
+    else:
+        print("Number is %d so %s" %(obj, found))
