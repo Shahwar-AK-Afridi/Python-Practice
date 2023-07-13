394c846afd0df2a9c60282037578110ea40fe84d
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2023-07-13 20:51:04 +0500

    Find The Smallest Number
    Find The Smallest Number
        1)Using for loop to read only
        2)Using min() function

--- Chapter 4(Loops)/Task10.py
+++ Chapter 4(Loops)/Task10.py
@@ -1,10 +1,28 @@
-""""Search an item Using Boolean Variable"""
+""""Find The Smallest Number
+    1)Using for loop to read only
+    2)Using min() function"""
 
-number = [9,41,12,3,74,15,98,44,57,32,16]
-for obj in number:
-    found = False        #Boolean Variable
-    if obj == 3:
-        found = True
-        print("Number is %d so Found: %s" %(obj, found))
-    else:
-        print("Number is %d so %s" %(obj, found))
+number = [22,44,76,98,56,33,55,11,23]
+
+print("----------Approach 1 using for loop to read only----------")
+def smallest(number):
+    try:
+        smallest_so_far = number[0]                          #We assume the list is not empty. This line initializes a variable called 'smallest_so_far' to the first element of the list.
+    except:
+        print("Empty List, Please Enter Again!")
+        quit()
+    for item in number:
+        if item < smallest_so_far:
+            smallest_so_far = item
+        else:
+            continue
+    return smallest_so_far                                   #This line returns the final value of the 'smallest_so_far' variable after the loop has finished
+
+smallest_so_far = smallest(number)
+print("Smallest Item:", smallest_so_far)
+
+print("-------------Approach 2 using min() function--------------")
+
+number = [22,44,76,98,56,33,55,3,23]
+smallest_so_far = min(number)
+print("Smallest Item:", smallest_so_far)
